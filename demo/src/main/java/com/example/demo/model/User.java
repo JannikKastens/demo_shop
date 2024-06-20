d79260b4d13f12399258b74a91b8bc64ae8c9ad7
Jannik Kastens <jannik.kastens@hmmh.de>
2024-06-20 11:14:29 +0200

    Add User, UserRepository, UserService and UserController

---
+++ demo/src/main/java/com/example/demo/model/User.java
@@ -0,0 +1,35 @@
+package com.example.demo.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(name = "email", nullable = false, unique = true)
+    private String email;
+
+    @Column(name = "password", nullable = false)
+    @JsonIgnore
+    private String password;
+
+    // private Role role;
+}
