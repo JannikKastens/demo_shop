b2e2561e5db945b50eeec39040cdbcf9a9538749
Jannik Kastens <119924776+JannikKastens@users.noreply.github.com>
2024-08-31 17:44:57 +0200

    Add basic header and global styles

--- Frontend/src/app/layout/header/header.component.ts
+++ Frontend/src/app/layout/header/header.component.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+import { NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [NgFor, RouterLink, RouterLinkActive],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.scss'
+  styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent {
-
+  navItems = [
+    { path: '/', label: 'Home' },
+    { path: '/products', label: 'Products' },
+    { path: '/cart', label: 'Cart' },
+    { path: '/user-profile', label: 'Profile' },
+  ];
 }
